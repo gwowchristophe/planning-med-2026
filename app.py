5f6100928b6d8c545d6c8409913ce3ad7733bfad
gwowchristophe <98766602+gwowchristophe@users.noreply.github.com>
2026-03-08 20:42:58 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import holidays
 # --- CONFIGURATION ---
 st.set_page_config(page_title="Planning Médical 2026", layout="wide")
 V = {"GW": 24, "GM": 24, "JK": 9, "JM": 7}
-URL_SHEET = "URL_DE_VOTRE_GOOGLE_SHEET_ICI" # À REMPLACER
+URL_SHEET = "https://docs.google.com/spreadsheets/d/1tk032kmegtMoTwhbOzopRns-NW4gVeyeuAe7CUmvbUE/edit?usp=sharing" # À REMPLACER
 
 # Connexion à Google Sheets
 conn = st.connection("gsheets", type=GSheetsConnection)
